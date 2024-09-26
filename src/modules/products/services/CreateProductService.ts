a84c124366b481a470a771d503c92bf46b75f32e
Guilherme Dilio de Souza <thekingguiiz2003@gmail.com>
2024-09-26 17:33:17 -0300

    feat: Implements the CreateProduct service

---
+++ src/modules/products/services/CreateProductService.ts
@@ -0,0 +1,38 @@
+import { getCustomRepository } from "typeorm"
+import { ProductRepository } from "../typeorm/repositories/ProductsRepository"
+import AppError from "@shared/errors/AppError"
+import Product from "../typeorm/entities/Product"
+
+// essa interface irá tipar as informações que estamos recebendo
+interface IRequest {
+  name: string,
+  price: number,
+  quantity: number
+}
+
+class CreateProductService {
+  public async execute({name, price, quantity}: IRequest): Promise<Product> { // IRequest -> Tipagem dos parâmetros
+    // método customizado então usa o método getCustomRepository
+    const productsRepository = getCustomRepository(ProductRepository)
+    // verifica se o produto existe com o mesmo nome, através do repositório de
+    // product que tem esse método customizado e mais os do TypeORM
+    const productExists = await productsRepository.findByName(name)
+
+    if(productExists) {
+      throw new AppError("There is already one product with this name")
+    }
+
+    // cria então uma instancia do produto com os
+    const product = productsRepository.create({
+      name,
+      price,
+      quantity
+    })
+
+    await productsRepository.save(product)
+
+    return product
+  }
+}
+
+export default CreateProductService
