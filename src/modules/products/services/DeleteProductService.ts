fd1a2e44cf23bfe005fe1faabfc398279b63dde0
Guilherme Dilio de Souza <thekingguiiz2003@gmail.com>
2024-09-26 18:51:27 -0300

    feat: Implements the delete product service

---
+++ src/modules/products/services/DeleteProductService.ts
@@ -0,0 +1,23 @@
+import { getCustomRepository } from "typeorm"
+import { ProductRepository } from "../typeorm/repositories/ProductsRepository"
+import AppError from "@shared/errors/AppError"
+
+interface IRequest {
+  id: string
+}
+
+class DeleteProductService {
+  public async execute({ id }: IRequest): Promise<void> {
+    const productsRepository = getCustomRepository(ProductRepository)
+
+    const product = await productsRepository.findOne(id)
+
+    if (!product) {
+      throw new AppError("Product not found")
+    }
+
+    await productsRepository.remove(product)
+  }
+}
+
+export default DeleteProductService
