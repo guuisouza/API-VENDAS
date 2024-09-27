b5be1f79e9ddc6396f52e31f5f656da465c6c244
Guilherme Dilio de Souza <thekingguiiz2003@gmail.com>
2024-09-27 17:30:03 -0300

    feat: Implements bcrypt in user creation to ensure password security

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -2,6 +2,7 @@ import { getCustomRepository } from "typeorm"
 import { UsersRepository } from "../typeorm/repositories/UsersRepository"
 import AppError from "@shared/errors/AppError"
 import User from "../typeorm/entities/User"
+import { hash } from "bcryptjs"
 
 // essa interface irá tipar as informações que estamos recebendo
 interface IRequest {
@@ -20,10 +21,12 @@ class CreateUserService {
       throw new AppError("Email address already used")
     }
 
+    const hashedPassword = await hash(password, 8)
+
     const user = usersRepository.create({
       name,
       email,
-      password
+      password: hashedPassword
     })
 
     await usersRepository.save(user)
