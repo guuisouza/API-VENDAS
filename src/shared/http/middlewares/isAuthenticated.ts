0834ac5007a5e972bed59c6c839168121913ec08
Guilherme Dilio de Souza <thekingguiiz2003@gmail.com>
2024-09-30 17:03:56 -0300

    feat: Adds the user id to every Request that passes through the authentication middleware

--- src/shared/http/middlewares/isAuthenticated.ts
+++ src/shared/http/middlewares/isAuthenticated.ts
@@ -3,6 +3,12 @@ import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 import authConfig from "@config/auth";
 
+interface TokenPayload {
+  iat: number,
+  exp: number,
+  sub: string
+}
+
 export default function isAuthenticated(
   req: Request,
   res: Response,
@@ -20,6 +26,12 @@ export default function isAuthenticated(
 
   try {
     const decodedToken = verify(token, authConfig.jwt.secret) // 1- token recebido 2- secret
+
+    const { sub } = decodedToken as TokenPayload
+
+    req.user = {
+      id: sub
+    }
 
     return next()
   } catch {
