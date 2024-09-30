2eca2dcf0fb7dd2a8466068626be73dbecf716bd
Guilherme Dilio de Souza <thekingguiiz2003@gmail.com>
2024-09-30 15:32:18 -0300

    feat: Implements the json web token when creating the user session to return an authentication token

--- src/modules/users/services/CreateSessionsService.ts
+++ src/modules/users/services/CreateSessionsService.ts
@@ -3,14 +3,20 @@ import { UsersRepository } from "../typeorm/repositories/UsersRepository"
 import AppError from "@shared/errors/AppError"
 import User from "../typeorm/entities/User"
 import { compare } from "bcryptjs"
+import { sign } from "jsonwebtoken"
 
 interface IRequest {
   email: string,
   password: string
 }
 
+interface IResponse {
+  user: User,
+  token: string
+}
+
 class CreateSessionsService {
-  public async execute({ email, password }: IRequest): Promise<User> { // IRequest -> Tipagem dos parâmetros
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
     const usersRepository = getCustomRepository(UsersRepository)
 
     const user = await usersRepository.findByEmail(email)
@@ -25,9 +31,12 @@ class CreateSessionsService {
       throw new AppError("Incorrect email/password combination.", 401)
     }
 
-    await usersRepository.save(user)
+    const token = sign({}, 'fbe3f1d1a6bd7b382733b6817dece23e', {
+      subject: user.id,
+      expiresIn: '1d'
+    })
 
-    return user
+    return {user, token}
   }
 }
 
