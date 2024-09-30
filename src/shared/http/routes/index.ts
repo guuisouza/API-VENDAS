0d381ce6e2bce586b854a7a5e9d312c8ff76c62e
Guilherme Dilio de Souza <thekingguiiz2003@gmail.com>
2024-09-30 14:22:38 -0300

    feat: Add and configure the post route for user sessions

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,10 +1,12 @@
 import productsRouter from "@modules/products/routes/ProductsRoutes";
 import usersRouter from "@modules/users/routes/UsersRoutes";
+import sessionsRouter from "@modules/users/routes/SessionsRoutes";
 import { Router } from "express";
 
 const routes = Router()
 
 routes.use('/products', productsRouter)
 routes.use('/users', usersRouter)
+routes.use('/sessions', sessionsRouter)
 
 export default routes
