e9c9b4ea8e4296da0ff6a1642a022eabca37f2d5
Guilherme Dilio de Souza <thekingguiiz2003@gmail.com>
2024-10-01 19:01:01 -0300

    feat: Change the template type to the html file that was created with handlebars

--- src/config/mail/HandlebarsMailTemplate.ts
+++ src/config/mail/HandlebarsMailTemplate.ts
@@ -1,20 +1,24 @@
 import handlebars from 'handlebars'
+import fs from 'fs'
 
 interface ITemplateVariable {
   [key: string]: string | number
 }
 
 interface IParseMailTemplate {
-  template: string,
+  file: string,
   variables: ITemplateVariable
 }
 
 export default class handlebarsMailTemplate {
   public async parse({
-    template,
+    file,
     variables
   }: IParseMailTemplate): Promise<string> {
-    const parseTemplate = handlebars.compile(template)
+    // fs para ler o arquivo
+    const templateFileContent = await fs.promises.readFile(file, {encoding: 'utf-8'})
+    // faz o parser do conteúdo que está sendo enviado por parametro
+    const parseTemplate = handlebars.compile(templateFileContent)
 
     return parseTemplate(variables)
   }
