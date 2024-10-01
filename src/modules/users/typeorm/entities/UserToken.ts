5d39e9b1083b98e8105a42fe6405cc29d9d4fc2f
Guilherme Dilio de Souza <thekingguiiz2003@gmail.com>
2024-10-01 15:35:16 -0300

    fix: Fix table name in entity annotation

--- src/modules/users/typeorm/entities/UserToken.ts
+++ src/modules/users/typeorm/entities/UserToken.ts
@@ -1,6 +1,6 @@
 import {Column, CreateDateColumn, Entity, Generated, PrimaryGeneratedColumn, UpdateDateColumn} from 'typeorm'
 
-@Entity('users_tokens')
+@Entity('user_tokens')
 class UserToken {
   @PrimaryGeneratedColumn('uuid')
   id: string
