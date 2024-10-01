e348f8fa409e97ff2ef533edf851dfdef50af99c
Guilherme Dilio de Souza <thekingguiiz2003@gmail.com>
2024-10-01 19:47:36 -0300

    feat: Implements the user profile controller

---
+++ src/modules/users/controllers/ProfileController.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from "express";
+import UpdateProfileService from "../services/UpdateProfileService";
+import ShowProfileService from "../services/ShowProfileService";
+
+export default class ProfileController {
+  public async show(req: Request, res: Response): Promise<Response> {
+    const showProfileService = new ShowProfileService()
+    const user_id = req.user.id
+
+    const user = await showProfileService.execute({ user_id })
+
+    return res.json(user)
+  }
+
+  public async update(req: Request, res: Response): Promise<Response> {
+    const {name, email, password, old_password} = req.body
+    const user_id = req.user.id
+
+
+    const updateProfile = new UpdateProfileService()
+
+    const user = await updateProfile.execute({
+      user_id,
+      name,
+      email,
+      password,
+      old_password
+    })
+
+    return res.json(user)
+  }
+}
