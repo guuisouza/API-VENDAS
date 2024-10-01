dfda787a5c25ca83385894538ed9da19ea32f54c
Guilherme Dilio de Souza <thekingguiiz2003@gmail.com>
2024-10-01 17:05:35 -0300

    feat: Implements routes for password management with celebrate validation

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,6 +1,7 @@
 import productsRouter from "@modules/products/routes/ProductsRoutes";
 import usersRouter from "@modules/users/routes/UsersRoutes";
 import sessionsRouter from "@modules/users/routes/SessionsRoutes";
+import passwordRouter from "@modules/users/routes/PasswordRoutes";
 import { Router } from "express";
 
 const routes = Router()
@@ -8,5 +9,6 @@ const routes = Router()
 routes.use('/products', productsRouter)
 routes.use('/users', usersRouter)
 routes.use('/sessions', sessionsRouter)
+routes.use('/password', passwordRouter)
 
 export default routes
