5b90451b3554d93acdf2d8c3524c888bfc24cc61
Guilherme Dilio de Souza <thekingguiiz2003@gmail.com>
2024-10-01 17:55:43 -0300

    feat: Implement ethereal mail in the send forgot password email service

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -2,6 +2,7 @@ import { getCustomRepository } from "typeorm"
 import AppError from "@shared/errors/AppError"
 import { UsersRepository } from "../typeorm/repositories/UsersRepository"
 import { UserTokensRepository } from "../typeorm/repositories/UserTokensRepository"
+import EtherealMail from "@config/mail/EtherealMail"
 
 interface IRequest {
   email: string,
@@ -20,8 +21,12 @@ class SendForgotPasswordEmailService {
 
     const token = await userTokenRepository.generate(user.id)
 
-    console.log(token)
+    // console.log(token)
 
+    await EtherealMail.sendMail({
+      to: email,
+      body: `Solicitação de redefinição de senha recebida: ${token?.token}`
+    })
   }
 }
 
