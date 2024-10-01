dbea8918d0b69eded3503560ee07a9ac6bcce443
Guilherme Dilio de Souza <thekingguiiz2003@gmail.com>
2024-10-01 19:01:51 -0300

    feat: Implement forgotPasswordTemplate in the send email service and add the link with the token

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,4 +1,5 @@
 import { getCustomRepository } from "typeorm"
+import path from "path"
 import AppError from "@shared/errors/AppError"
 import { UsersRepository } from "../typeorm/repositories/UsersRepository"
 import { UserTokensRepository } from "../typeorm/repositories/UserTokensRepository"
@@ -21,7 +22,12 @@ class SendForgotPasswordEmailService {
 
     const token = await userTokenRepository.generate(user.id)
 
-    // console.log(token)
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot_password.hbs'
+    )
 
     await EtherealMail.sendMail({
       to: {
@@ -30,10 +36,10 @@ class SendForgotPasswordEmailService {
       },
       subject: '[API VENDAS] Recuperação de senha',
       templateData: {
-        template: `Olá {{name}}: {{token}}`,
+        file: forgotPasswordTemplate,
         variables: {
           name: user.name,
-          token: token?.token
+          link: `http://localhost:3000/reset_password?token=${token.token}`
         }
       }
     })
