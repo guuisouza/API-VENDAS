b77f1b02bdf66cb4e1e7a5ed52ba58a0c19570a0
Guilherme Dilio de Souza <thekingguiiz2003@gmail.com>
2024-10-01 14:04:41 -0300

    feat: Implements the Reset Password Service

---
+++ src/modules/users/services/ResetPasswordService.ts
@@ -0,0 +1,42 @@
+import { getCustomRepository } from "typeorm"
+import AppError from "@shared/errors/AppError"
+import { UsersRepository } from "../typeorm/repositories/UsersRepository"
+import { UserTokensRepository } from "../typeorm/repositories/UserTokensRepository"
+import {isAfter, addHours} from 'date-fns'
+import { hash } from 'bcryptjs'
+
+interface IRequest {
+  token: string,
+  password: string
+}
+
+class ResetPasswordService {
+  public async execute({ token, password }: IRequest): Promise<void> {
+    const usersRepository = getCustomRepository(UsersRepository)
+    const userTokenRepository = getCustomRepository(UserTokensRepository)
+
+    const userToken = await userTokenRepository.findByToken(token)
+
+    if (!userToken) {
+      throw new AppError('User token does not exists.')
+    }
+
+    const user = await usersRepository.findById(userToken.user_id)
+
+    if (!user) {
+      throw new AppError('User does not exists.')
+    }
+
+    // verificar se o token ainda está com validade - date-fns
+    const tokenCreatedAt = userToken.created_at
+    const compareDate = addHours(tokenCreatedAt, 2)
+
+    if (isAfter(Date.now(), compareDate)){
+      throw new AppError("Token expired.")
+    }
+
+    user.password = await hash(password, 8)
+  }
+}
+
+export default ResetPasswordService
