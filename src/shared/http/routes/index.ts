7b8468bf6708d51c26c213a4e2987772802b8857
Guilherme Dilio de Souza <thekingguiiz2003@gmail.com>
2024-10-01 19:48:36 -0300

    feat: Implements controller profile routes with validation by celebrate

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -3,6 +3,7 @@ import usersRouter from "@modules/users/routes/UsersRoutes";
 import sessionsRouter from "@modules/users/routes/SessionsRoutes";
 import passwordRouter from "@modules/users/routes/PasswordRoutes";
 import { Router } from "express";
+import profileRouter from "@modules/users/routes/ProfileRoutes";
 
 const routes = Router()
 
@@ -10,5 +11,6 @@ routes.use('/products', productsRouter)
 routes.use('/users', usersRouter)
 routes.use('/sessions', sessionsRouter)
 routes.use('/password', passwordRouter)
+routes.use('/profile', profileRouter)
 
 export default routes
