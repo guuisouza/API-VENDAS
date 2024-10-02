aa7d1c3642c5c945096a741b100859b668f8db94
Guilherme Dilio de Souza <thekingguiiz2003@gmail.com>
2024-10-02 15:18:47 -0300

    feat: Adds the foreign key order id and product id to the pivot table

---
+++ src/shared/typeorm/migrations/1727892164520-AddProductIdToOrdersProducts.ts
@@ -0,0 +1,27 @@
+import {MigrationInterface, QueryRunner, TableColumn, TableForeignKey} from "typeorm";
+
+export class AddProductIdToOrdersProducts1727892164520 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.addColumn('orders_products', new TableColumn({
+        name: 'product_id',
+        type: 'uuid',
+        isNullable: true
+      }))
+
+      await queryRunner.createForeignKey('orders_products', new TableForeignKey({
+        name: 'OrdersProductsProduct', // um ou mais registros de uma mesma order
+        columnNames: ['product_id'],
+        referencedTableName: 'products',
+        referencedColumnNames: ['id'],
+        onDelete: 'SET NULL' // campo order_id ficará nulo
+      }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropForeignKey('orders_products', 'OrdersProductsProduct')
+
+      await queryRunner.dropColumn('orders_products', 'product_id')
+    }
+
+}
