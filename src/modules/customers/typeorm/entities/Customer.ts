19e4f479b666570401c4fd32f547a092494b7bdf
Guilherme Dilio de Souza <thekingguiiz2003@gmail.com>
2024-10-02 10:52:14 -0300

    refactor: renames the Customer entity class name to the singular

--- src/modules/customers/typeorm/entities/Customer.ts
+++ src/modules/customers/typeorm/entities/Customer.ts
@@ -1,7 +1,7 @@
 import {Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn} from 'typeorm'
 
 @Entity('customers')
-class Customers {
+class Customer {
   @PrimaryGeneratedColumn('uuid')
   id: string
 
@@ -18,4 +18,4 @@ class Customers {
   updated_at: Date
 }
 
-export default Customers
+export default Customer
