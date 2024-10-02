d0769445e96c731bba58c7fce396ef2b5a5a3f55
Guilherme Dilio de Souza <thekingguiiz2003@gmail.com>
2024-10-02 16:03:16 -0300

    feat: Add a link to the Orders Products pivot table in the product entity

--- src/modules/products/typeorm/entities/Product.ts
+++ src/modules/products/typeorm/entities/Product.ts
@@ -1,9 +1,13 @@
-import {Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn} from 'typeorm'
+import {Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn} from 'typeorm'
+import OrdersProducts from '@modules/orders/typeorm/entities/OrdersProducts'
 
 @Entity('products') // mapeamento na tabela products
 class Product {
   @PrimaryGeneratedColumn('uuid')
   id: string
+
+  @OneToMany(() => OrdersProducts, order_products => order_products.product)
+  order_products: OrdersProducts[]
 
   @Column()
   name: string
