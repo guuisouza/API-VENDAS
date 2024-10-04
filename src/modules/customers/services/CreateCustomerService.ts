f9a4e819b8ec5d802b2d9aef42f01efafdb7dd15
Guilherme Dilio de Souza <thekingguiiz2003@gmail.com>
2024-10-04 14:11:02 -0300

    refactor: applying the concept of Dependency Inversion in the create customer service

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -1,30 +1,24 @@
-import { getCustomRepository } from "typeorm"
-import { CustomersRepository } from "../infra/typeorm/repositories/CustomersRepository"
 import AppError from "@shared/errors/AppError"
-import Customer from "../infra/typeorm/entities/Customer"
-
-// essa interface irá tipar as informações que estamos recebendo
-interface IRequest {
-  name: string,
-  email: string,
-}
+import { ICustomersRepository } from "../domain/repositories/ICustomersRepository"
+import { ICreateCustomer } from "../domain/models/ICreateCustomer"
+import { ICustomer } from "../domain/models/ICustomer"
 
 class CreateCustomerService {
-  public async execute({ name, email }: IRequest): Promise<Customer> { // IRequest -> Tipagem dos parâmetros
-    const customersRepository = getCustomRepository(CustomersRepository)
+  constructor(private customersRepository: ICustomersRepository) {
 
-    const emailExists = await customersRepository.findByEmail(email)
+  }
+
+  public async execute({ name, email }: ICreateCustomer): Promise<ICustomer> {
+    const emailExists = await this.customersRepository.findByEmail(email)
 
     if (emailExists) {
       throw new AppError("Email address already used")
     }
 
-    const customer = customersRepository.create({
+    const customer = this.customersRepository.create({
       name,
       email
     })
-
-    await customersRepository.save(customer)
 
     return customer
   }
