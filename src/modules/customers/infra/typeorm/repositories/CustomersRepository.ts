656ec484183e7fdf99cc70cc9bb5e6a5eb46ddd5
Guilherme Dilio de Souza <thekingguiiz2003@gmail.com>
2024-10-03 23:07:31 -0300

    refactor: Unlinks the customers repository from the typeorm's default Repository class and implements the repository from what was defined in the domain layer

--- src/modules/customers/infra/typeorm/repositories/CustomersRepository.ts
+++ src/modules/customers/infra/typeorm/repositories/CustomersRepository.ts
@@ -1,12 +1,33 @@
-import { EntityRepository, Repository } from "typeorm";
+import { getRepository, Repository } from "typeorm";
 import Customer from "../entities/Customer";
+import { ICustomersRepository } from "@modules/customers/domain/repositories/ICustomersRepository";
+import { ICreateCustomer } from "@modules/customers/domain/models/ICreateCustomer";
 
-@EntityRepository(Customer)
-export class CustomersRepository extends Repository<Customer> {
-  // metodos que precisamos customizar
+export class CustomersRepository implements ICustomersRepository {
+  private ormRepository: Repository<Customer> // o generics indica o que será manipulado
+  constructor() {
+    this.ormRepository = getRepository(Customer)
+  }
+
+  public async create({name, email}: ICreateCustomer): Promise<Customer> {
+    const customer = this.ormRepository.create({
+      name,
+      email
+    })
+
+    await this.ormRepository.save(customer)
+
+    return customer
+  }
+
+  public async save(customer: Customer): Promise<Customer> {
+    await this.ormRepository.save(customer)
+
+    return customer
+  }
 
   public async findByName(name: string): Promise<Customer | undefined> {
-    const customer = await this.findOne({
+    const customer = await this.ormRepository.findOne({
       where: {
         name,
       }
@@ -16,7 +37,7 @@ export class CustomersRepository extends Repository<Customer> {
   }
 
   public async findById(id: string): Promise<Customer | undefined> {
-    const customer = await this.findOne({
+    const customer = await this.ormRepository.findOne({
       where: {
         id,
       }
@@ -26,7 +47,7 @@ export class CustomersRepository extends Repository<Customer> {
   }
 
   public async findByEmail(email: string): Promise<Customer | undefined> {
-    const customer = await this.findOne({
+    const customer = await this.ormRepository.findOne({
       where: {
         email,
       }
