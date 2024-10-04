90fcf89f689192ed049ed17f9d9f8e987f79fdd3
Guilherme Dilio de Souza <thekingguiiz2003@gmail.com>
2024-10-04 15:20:20 -0300

    refactor: Implement the dependency container in our main file

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -7,6 +7,7 @@ import { errors } from 'celebrate';
 import { pagination } from 'typeorm-pagination'
 import routes from "./routes"
 import AppError from "@shared/errors/AppError";
+import '@shared/container'
 import '@shared/infra/typeorm'
 import uploadConfig from '@config/upload'
 
