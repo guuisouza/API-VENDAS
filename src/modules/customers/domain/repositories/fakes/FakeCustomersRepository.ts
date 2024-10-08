8bef377a099fbb0aacee646fca4f5aa7f02bb02d
Guilherme Dilio de Souza <thekingguiiz2003@gmail.com>
2024-10-08 16:43:30 -0300

    test: Implements TDD for CreateCustomerService

---
+++ src/modules/customers/domain/repositories/fakes/FakeCustomersRepository.ts
@@ -0,0 +1,47 @@
+// import Customer from "@modules/customers/";
+// import { v4 as uuidv4 } from "uuid";
+// import { ICustomersRepository } from "@modules/customers/domain/repositories/ICustomersRepository";
+// import { ICreateCustomer } from "@modules/customers/domain/models/ICreateCustomer";
+
+
+// export class FakeCustomersRepository implements ICustomersRepository {
+//   private customers: Customer[] = []
+
+
+//   public async create({name, email}: ICreateCustomer): Promise<Customer> {
+//     const customer = new Customer()
+
+//     customer.id = uuidv4()
+//     customer.name = name
+//     customer.email = email
+
+//     this.customers.push(customer)
+
+
+
+//   }
+
+  // public async save(customer: Customer): Promise<Customer> {
+
+  // }
+
+  // public async remove(customer: Customer): Promise<void> {
+
+  // }
+
+  // public async findAll(): Promise<Customer | undefined> {
+
+  // }
+
+  // public async findByName(name: string): Promise<Customer | undefined> {
+
+  // }
+
+  // public async findById(id: string): Promise<Customer | undefined> {
+
+  // }
+
+  // public async findByEmail(email: string): Promise<Customer | undefined> {
+
+  // }
+// }
---
+++ src/modules/customers/services/CreateCustomerService.spec.ts
@@ -0,0 +1,9 @@
+describe('CreateCustomer', () => {
+  it('should be able to create a new customer', () =>{
+    expect(1).toBe(1)
+  })
+
+  it('should not be able to create two customers with the same email', () =>{
+    expect(1).toBe(2)
+  })
+})
